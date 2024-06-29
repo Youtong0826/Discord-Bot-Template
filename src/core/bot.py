d953feb2ff8b0fd2003ea6ed8f3b17c1d1bd7011
Youtong0826 <jimmy2007826@gmail.com>
2024-06-30 02:00:12 +0800

    add interaction method

--- src/core/bot.py
+++ src/core/bot.py
@@ -35,3 +35,33 @@ class Bot(Bot):
     def __init__(self, description=None, *args, **options):
         super().__init__(description, *args, **options)
         self.token = os.getenv("TOKEN")
+        
+    def get_interaction_value(self, interaction: Interaction):
+        return [data.get("components",{})[0].get("value") for data in interaction.data.get("components",{})]
+    
+    def get_select_value(self, interaction: Interaction, index: int = -1) -> Union[Any, list[Any]]:
+        return interaction.data.get("values")[index] if index != -1 else interaction.data.get("values")
+    
+    def from_component(self, view: View, component: Union[ActionRow, DiscordButton, SelectMenu]):
+        kwargs = component.to_dict()
+        kwargs.pop('type')
+        
+        match component.type:
+            case ComponentType.button:
+                view.add_item(Button(**kwargs))
+                
+            case ComponentType.select:
+                view.add_item(Select(**kwargs))
+                
+            case ComponentType.role_select:
+                view.add_item(Select(select_type=ComponentType.role_select, **kwargs))
+                
+            case ComponentType.user_select:
+                view.add_item(Select(select_type=ComponentType.user_select, **kwargs))
+                
+            case ComponentType.channel_select:
+                view.add_item(Select(select_type=ComponentType.channel_select, **kwargs))
+                
+            case _:
+                for c in component.children:
+                    self.from_component(view, c)
