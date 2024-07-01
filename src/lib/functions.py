e6a2d02f095ba7bd716d3a79b2f2daae07f57506
Youtong0826 <jimmy2007826@gmail.com>
2024-07-01 09:35:58 +0800

    fix bug

--- src/lib/functions.py
+++ src/lib/functions.py
@@ -4,6 +4,6 @@ from datetime import (
     timezone
 )
 
-def get_now_time(self, time: datetime = None, hours = 8) -> datetime:
+def get_now_time(time: datetime = None, hours = 8) -> datetime:
     ori = datetime.now(timezone(timedelta(hours=hours))) if not time else time
     return datetime(ori.year, ori.month, ori.day, ori.hour, ori.minute, ori.second)
